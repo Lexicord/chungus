d075e7224e8f8a1632d55c1960e8dba1f5935cf4
Lexicord <84813757+Lexicord@users.noreply.github.com>
2021-07-20 14:16:28 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -804,7 +804,10 @@ class PlayState extends MusicBeatState
 
 			case 'mall':
 				boyfriend.x += 200;
-
+			case 'chungusland':
+				boyfriend.y -= 175
+				dad.y -= 175
+				gf.y -= 175
 			case 'mallEvil':
 				boyfriend.x += 320;
 				dad.y -= 80;
